867ec11a51cbd7628ba863f543080337ef3ecb00
Aidan O'Grady <aidan@aidanogrady.com>
2016-10-21 14:20:50 +0100

    Added maven project, with logback and application class.

---
+++ src/main/java/com/aidanogrady/cs547/assignment03/Application.java
@@ -0,0 +1,15 @@
+package com.aidanogrady.cs547.assignment03;
+
+/**
+ * Main application class for the system.
+ *
+ * @author Aidan O'Grady
+ * @since 0.0
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("CS547 Assignment 03: Requirements Prioritisation");
+        System.out.println("Author: Aidan O'Grady (201218150)");
+        System.out.println();
+    }
+}
